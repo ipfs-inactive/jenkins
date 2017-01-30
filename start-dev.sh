c62bd09c79cf0f39efda28b6837a86d13f1b50f4
Jakub Sztandera <kubuxu@protonmail.ch>
2017-01-30 16:56:06 +0100

    Change docker local port to less comonly used.

--- start-dev.sh
+++ start-dev.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 docker build -t ipfs/jenkins:latest .
-docker run -p 8080:8080 -v $(pwd)/config:/var/jenkins_home ipfs/jenkins:latest
+docker run -p 8090:8080 -v $(pwd)/config:/var/jenkins_home ipfs/jenkins:latest
