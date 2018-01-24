3bf96f47a5d7ec754a9ea591ced2bfaf084f6525
Victor Bjelkholm <victorbjelkholm@gmail.com>
2018-01-24 11:52:10 +0100

    Extract windows workers into it's own tf module

--- main.tf
+++ main.tf
@@ -111,35 +111,6 @@ resource "aws_key_pair" "victor-ssh" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDMOBeiZnugxLNgt4sJZPMVzrW3sMpkB2PFv9V4ESW5FZEJPsV0Q09XAfFQL8RxWB0UFMZk43lqImfXoxpLrMyAOa2/sco/2r0uEGtLscYAg6HwCuaXnZeuMwByYIrUSfmZPd7mGo1GYqP5gVfuaKkAVnIplXK5khQL4Ix+aJADDmUdWrBWVeP4KlqfDWM7DCcc8nF9+8C8Wu6uE5a8Zn2c25laML472F3havXysj8lp+VRz2KOSSpVYLOifYajbQH2GaxuynLOny6+vOIVO1LQf5Do+RgWT70sWUdb9S+kjwqlijFUTTvzXuA5cSHReS8h9wtcSRra4qlWpcGr0O0BET1o7CWJXbmmBhtsj+yjR0rR5Xt5/tqEVrHCImdL+ggDmn4wQbRDCRTO6EcnZNiPgdRuve73gguzAFKCINMId3L/ttqOnjn8Bjis046ypKwvSvkan75tJ3/ZpMYSCop51ULdPG8UvdJjH6x75e94S8iH7UYU5c1gFXE+ciukkyyje2ldoaD3zZLUFAWc7XZSZ6iQCvEQCZx32suqgbBzQ4jgoLuxBY7Lpe2sedYGbixBGALgd7jbzG+3NwQeNFOcifbJ/ncPdtpIuYYsKzWtxcJSeOiWqzZWaSkHIqP4TGOgd9GNgedmg/AeeubgDqkN+wI5wy/DynZb0jdtzOZfSQ== victor@niue"
 }
 
-resource "aws_security_group" "jenkins_windows" {
-  name        = "jenkins_windows"
-  description = "Allow inbound WinRM/RDP and everything outbound"
-
-  # WinRM
-  ingress {
-    from_port   = 5985
-    to_port     = 5986
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # RDP
-  ingress {
-    from_port   = 3389
-    to_port     = 3389
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # All allowed outbound
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_security_group" "jenkins_master" {
   name        = "jenkins_master"
   description = "Allow inbound ssh traffic/everything outbound"
@@ -209,51 +180,19 @@ module "linux-workers" {
   jenkins_master_ip = "${aws_instance.jenkins_master.0.public_ip}"
 }
 
-resource "aws_instance" "windows" {
-  security_groups             = ["${aws_security_group.jenkins_windows.name}"]
-  ami                         = "${var.windows_ami}"
-  instance_type               = "${var.windows_type}"
-  associate_public_ip_address = true
-  key_name                    = "victor-ssh-key"
-  count                       = "${terraform.workspace == "default" ? var.windows_count : 1}"
-  tags { Name = "worker-windows" }
-
-  connection {
-    type = "winrm"
-    user = "Administrator"
-
-    password = "${var.windows_admin_password}"
-
-    # set from default of 5m to 10m to avoid winrm timeout
-    timeout = "30m"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "@\"%SystemRoot%\\System32\\WindowsPowerShell\\v1.0\\powershell.exe\" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command \"iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))\" && SET \"PATH=%PATH%;%ALLUSERSPROFILE%\\chocolatey\\bin\"",
-      "choco install -y wget jre8 git nssm googlechrome python2 python3 vcredist2015 make nodejs microsoft-visual-cpp-build-tools rktools.2003",
-      "npm install --verbose --global --production windows-build-tools",
-      "git config --global core.autocrlf input",
-      "wget https://repo.jenkins-ci.org/releases/org/jenkins-ci/plugins/swarm-client/${var.swarm_version}/swarm-client-${var.swarm_version}.jar",
-      "nssm install swarm java -jar C:\\Users\\Administrator\\swarm-client-${var.swarm_version}.jar -master http://${aws_instance.jenkins_master.0.public_ip}:8080 -password ${var.jenkins_password} -username ${var.jenkins_username} -tunnel ${aws_instance.jenkins_master.0.public_ip}:50000 -labels ${var.windows_jenkins_worker_labels} -name ${var.windows_jenkins_worker_name} -fsroot ${var.windows_jenkins_worker_fsroot} -mode exclusive -executors 1",
-      "nssm start swarm",
-    ]
-  }
-
-  root_block_device {
-    volume_size = "100"
-  }
-
-  user_data = <<EOF
-<powershell>
-  winrm --% quickconfig -q & winrm set winrm/config @{MaxTimeoutms="1800000"} & winrm set winrm/config/service @{AllowUnencrypted="true"} & winrm set winrm/config/service/auth @{Basic="true"}
-  netsh advfirewall firewall add rule name="WinRM in" protocol=TCP dir=in profile=any localport=5985 remoteip=any localip=any action=allow
-  # Set Administrator password
-  $admin = [adsi]("WinNT://./administrator, user")
-  $admin.psbase.invoke("SetPassword", "${var.windows_admin_password}")
-  Set-MpPreference -DisableRealtimeMonitoring $true
-</powershell>
-EOF
+module "windows-workers" {
+  source = "./windows-workers"
+  swarm_version = "${var.swarm_version}"
+  jenkins_username = "${var.jenkins_username}"
+  jenkins_password = "${var.jenkins_password}"
+  windows_admin_password = "${var.windows_admin_password}"
+  windows_ami = "${var.windows_ami}"
+  windows_type = "${var.windows_type}"
+  windows_count = "${var.windows_count}"
+  windows_jenkins_worker_labels = "${var.windows_jenkins_worker_labels}"
+  windows_jenkins_worker_name = "${var.windows_jenkins_worker_name}"
+  windows_jenkins_worker_fsroot = "${var.windows_jenkins_worker_fsroot}"
+  jenkins_master_ip = "${aws_instance.jenkins_master.0.public_ip}"
 }
 
 resource "dnsimple_record" "jenkins_domain" {
@@ -383,5 +322,5 @@ output "linux_ips" {
 }
 
 output "windows_ips" {
-  value = "${aws_instance.windows.*.public_ip}"
+  value = "${module.windows-workers.ips}"
 }
---
+++ windows-workers/main.tf
@@ -0,0 +1,95 @@
+variable "swarm_version" {}
+
+variable "jenkins_username" {}
+variable "jenkins_password" {}
+
+variable "windows_admin_password" {}
+
+variable "windows_ami" {}
+variable "windows_type" {}
+variable "windows_count" {}
+variable "windows_jenkins_worker_labels" {}
+variable "windows_jenkins_worker_name" {}
+variable "windows_jenkins_worker_fsroot" {}
+
+variable "jenkins_master_ip" {}
+
+resource "aws_security_group" "jenkins_windows" {
+  name        = "jenkins_windows"
+  description = "Allow inbound WinRM/RDP and everything outbound"
+
+  # WinRM
+  ingress {
+    from_port   = 5985
+    to_port     = 5986
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # RDP
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # All allowed outbound
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "windows" {
+  security_groups             = ["${aws_security_group.jenkins_windows.name}"]
+  ami                         = "${var.windows_ami}"
+  instance_type               = "${var.windows_type}"
+  associate_public_ip_address = true
+  key_name                    = "victor-ssh-key"
+  count                       = "${terraform.workspace == "default" ? var.windows_count : 1}"
+  tags { Name = "worker-windows" }
+
+  connection {
+    type = "winrm"
+    user = "Administrator"
+
+    password = "${var.windows_admin_password}"
+
+    # set from default of 5m to 10m to avoid winrm timeout
+    timeout = "30m"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "@\"%SystemRoot%\\System32\\WindowsPowerShell\\v1.0\\powershell.exe\" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command \"iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))\" && SET \"PATH=%PATH%;%ALLUSERSPROFILE%\\chocolatey\\bin\"",
+      "choco install -y wget jre8 git nssm googlechrome python2 python3 vcredist2015 make nodejs microsoft-visual-cpp-build-tools rktools.2003",
+      "npm install --verbose --global --production windows-build-tools",
+      "git config --global core.autocrlf input",
+      "wget https://repo.jenkins-ci.org/releases/org/jenkins-ci/plugins/swarm-client/${var.swarm_version}/swarm-client-${var.swarm_version}.jar",
+      "nssm install swarm java -jar C:\\Users\\Administrator\\swarm-client-${var.swarm_version}.jar -master http://${var.jenkins_master_ip}:8080 -password ${var.jenkins_password} -username ${var.jenkins_username} -tunnel ${var.jenkins_master_ip}:50000 -labels ${var.windows_jenkins_worker_labels} -name ${var.windows_jenkins_worker_name} -fsroot ${var.windows_jenkins_worker_fsroot} -mode exclusive -executors 1",
+      "nssm start swarm",
+    ]
+  }
+
+  root_block_device {
+    volume_size = "100"
+  }
+
+  user_data = <<EOF
+<powershell>
+  winrm --% quickconfig -q & winrm set winrm/config @{MaxTimeoutms="1800000"} & winrm set winrm/config/service @{AllowUnencrypted="true"} & winrm set winrm/config/service/auth @{Basic="true"}
+  netsh advfirewall firewall add rule name="WinRM in" protocol=TCP dir=in profile=any localport=5985 remoteip=any localip=any action=allow
+  # Set Administrator password
+  $admin = [adsi]("WinNT://./administrator, user")
+  $admin.psbase.invoke("SetPassword", "${var.windows_admin_password}")
+  Set-MpPreference -DisableRealtimeMonitoring $true
+</powershell>
+EOF
+}
+
+output "ips" {
+  value = "${aws_instance.windows.*.public_ip}"
+}
