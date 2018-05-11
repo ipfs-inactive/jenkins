fc2168688b053e7ff90c8c3555e061a96628610d
Victor Bjelkholm <victorbjelkholm@gmail.com>
2018-05-11 22:37:43 +0200

    Update setup-auth script path
    License: MIT
    Signed-off-by: Victor Bjelkholm <git@victor.earth>

--- config/setup-auth.sh
+++ config/setup-auth.sh
@@ -5,5 +5,5 @@ set -e
 export USERNAME=immutablejenkins
 export PASSWORD=$(cat /tmp/userauthtoken)
 TOKEN=$(curl --user "$USERNAME:$PASSWORD" -s http://localhost:8080/crumbIssuer/api/json | python -c 'import sys,json;j=json.load(sys.stdin);print j["crumbRequestField"] + "=" + j["crumb"]')
-curl --user "$USERNAME:$PASSWORD" -d "$TOKEN" --data-urlencode "script=$(</var/lib/jenkins/setup-auth.groovy)" http://localhost:8080/scriptText
+curl --user "$USERNAME:$PASSWORD" -d "$TOKEN" --data-urlencode "script=$(</efs/lib/jenkins/setup-auth.groovy)" http://localhost:8080/scriptText
 
